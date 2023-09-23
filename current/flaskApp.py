4625a994bbbbd0ff5f61a6aee1364b3dfb957f3f
KChoiSlattery <kchoislattery@gatech.edu>
2023-09-23 04:23:06 -0400

    Fixed some parsing problems with the normal algebraic solver

--- current/flaskApp.py
+++ current/flaskApp.py
@@ -24,18 +24,18 @@ def solveSingleVar():
     tex_solve_var = request.json['solve_var']
     try:
         #sanitize input by changing "d" to "\mathrm{d}"
-        tex_equation = tex_equation.replace("d",r"d_{actuallyD}")
-        sympy_solve_var = latex2sympy2.latex2sympy(tex_solve_var)
+        tex_equation = sanitize_tex(tex_equation)
+        sympy_solve_var = latex2sympy2.latex2sympy(sanitize_tex(tex_solve_var))
         inlhs = latex2sympy2.latex2sympy(tex_equation[:tex_equation.index("=")])
         inrhs = latex2sympy2.latex2sympy(tex_equation[tex_equation.index("=")+1:])
         sympy_equation = sp.Eq(inlhs,inrhs)
         free_symbols = sympy_equation.free_symbols
         sympy_solved_rhs = sp.solve(sympy_equation,sympy_solve_var)
-        tex_solved_rhs = sp.latex(sympy_solved_rhs, inv_trig_style="power").replace(r"d_{actuallyD}","d")
+        tex_solved_rhs = desanitize_tex(sp.latex(sympy_solved_rhs, inv_trig_style="power"))
         if len(sympy_solved_rhs) == 1:
-            octave_code = sp.printing.octave_code(sympy_solved_rhs[0],assign_to=sympy_solve_var).replace(r"d_{actuallyD}","d")
-            python_code = sp.printing.pycode(sympy_solved_rhs[0]).replace(r"d_actuallyD","d")
-            c_code = sp.printing.ccode(sympy_solved_rhs[0],assign_to=sympy_solve_var).replace(r"d_{actuallyD}","d")
+            octave_code = sp.printing.octave_code(sympy_solved_rhs[0],assign_to=sympy_solve_var)
+            python_code = sp.printing.pycode(sympy_solved_rhs[0])
+            c_code = sp.printing.ccode(sympy_solved_rhs[0],assign_to=sympy_solve_var)
         else:
             numSolutionsInputted = 0
             octave_code = ""
@@ -47,9 +47,9 @@ def solveSingleVar():
                 else: 
                     endingNewline = ""
                 numSolutionsInputted = numSolutionsInputted + 1
-                octave_code = octave_code + r"%Solution " + str(numSolutionsInputted) + ":\n" + sp.printing.octave_code(i,assign_to=sympy_solve_var).replace(r"d_{actuallyD}","d")+endingNewline
-                python_code = python_code + r"#Solution " + str(numSolutionsInputted) + ":\n" + sp.printing.pycode(sympy_solve_var) + " = " + sp.printing.pycode(i).replace(r"d_actuallyD","d")+endingNewline
-                c_code = c_code + r"//Solution " + str(numSolutionsInputted) + ":\n" + sp.printing.ccode(i,assign_to=sympy_solve_var).replace(r"d_{actuallyD}","d")+endingNewline
+                octave_code = octave_code + r"%Solution " + str(numSolutionsInputted) + ":\n" + desanitize_tex(sp.printing.octave_code(i,assign_to=sympy_solve_var).replace(r"d_{actuallyD}","d"))+endingNewline
+                python_code = python_code + r"#Solution " + str(numSolutionsInputted) + ":\n" + desanitize_tex(sp.printing.pycode(sympy_solve_var) + " = " + sp.printing.pycode(i))+endingNewline
+                c_code = c_code + r"//Solution " + str(numSolutionsInputted) + ":\n" + desanitize_tex(sp.printing.ccode(i,assign_to=sympy_solve_var))+endingNewline
         return json.dumps({
             "free_symbols": [str(element) for element in free_symbols],
             "solved_rhs": tex_solved_rhs,
